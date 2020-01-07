f9cab0abe7936c55c2a0ea590df2e31a4070b6a0
Zach Cullen <zacullen@gmail.com>
2020-01-07 19:09:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    error_msg = 'Transaction rejected.Please check your account balance.'
+    error_msg = 'Transaction rejected. Please check your account balance.'
     if valid? && @sender.balance >= @amount && @status == 'pending'
       @sender.balance -= @amount
       @receiver.deposit(@amount)
