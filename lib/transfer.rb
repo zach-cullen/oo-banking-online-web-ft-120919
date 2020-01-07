14a08701630278b81abf6fb7b2382d912e55b95f
Zach Cullen <zacullen@gmail.com>
2020-01-07 19:06:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    valid? && @sender.balance >= @amount && @status == 'pending'
+    if valid? && @sender.balance >= @amount && @status == 'pending'
       @sender.balance -= @amount
       @receiver.deposit(@amount)
       @status = 'complete'
